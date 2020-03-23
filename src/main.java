895c750d4bf0ae31ad945a9e0a74477dbadda4b2
Juan Thielmann <meganitrospeed@gmail.com>
2020-03-23 22:33:47 +0000

    Arreglado un bug que no contaba las iteraciones Cambiada la formula matematica Aprobada esta tarea

--- src/main.java
+++ src/main.java
@@ -3,15 +3,16 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
 int i;
-int n = 0;
+int ih = 0;
+double n = 0;
 System.out.println(" Introduce la cantidad de iteraciones");
 Scanner texto = new Scanner(System.in);
 i = texto.nextInt();
 System.out.println("Introduce el valor de n");
 n = texto.nextInt();
-        while (i != n) {
-            i++;
-            n = n+n;
+        while (i != ih ) {
+            ih++;
+            n = (n + Math.pow(n,2))/2;
             System.out.println(n);
         }
     }
